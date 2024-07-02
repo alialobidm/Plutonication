34a2dd4612f7d8fa52a61c0c214aa8b2dd65f6c5
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2024-07-02 18:17:48 +0200

    test fixed

--- packages/tests/tests/AccessCredentials.spec.ts
+++ packages/tests/tests/AccessCredentials.spec.ts
@@ -2,5 +2,5 @@ import { accessCredentials } from "./helper";
 import { test, expect } from '@playwright/test';
 
 test('AccessCredentials to uri', async () => {
-  expect(accessCredentials.ToUri()).toBe(`plutonication:?url=wss%3A%2F%2Fplutonication-acnha.ondigitalocean.app%2F&key=${accessCredentials.key}&name=Plutonication%20test&icon=https%3A%2F%2Frostislavlitovkin.pythonanywhere.com%2Fplutowalleticonwhite`)
+  expect(accessCredentials.ToUri()).toBe(`plutonication:?url=wss%3A%2F%2Fplutonication.com%2F&key=${accessCredentials.key}&name=Plutonication%20test&icon=https%3A%2F%2Frostislavlitovkin.pythonanywhere.com%2Fplutowalleticonwhite`)
 });
